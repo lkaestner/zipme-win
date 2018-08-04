54392c765aceff0f311e6853c8996e83d6dead2d
Lukas Kästner <connect@lkaestner.com>
2018-08-04 21:12:57 +0200

    initial commit

---
+++ zipme.bat
@@ -0,0 +1,13 @@
+@echo off
+
+for /f "delims=" %%a in ('wmic OS Get localdatetime  ^| find "."') do set "dt=%%a"
+set "YYYY=%dt:~0,4%"
+set "MM=%dt:~4,2%"
+set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%"
+set "Min=%dt:~10,2%"
+set "Sec=%dt:~12,2%"
+
+set filename=backup_%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%.zip
+
+7z a "%filename%" "%~dp0/*" -x!*.zip
